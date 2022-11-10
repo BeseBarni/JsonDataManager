c20b76e67b4c8da5c7967145bc82bf471d313a07
Barnabás Bese <besebarni@gmail.com>
2022-11-10 08:16:15 +0100

    Add project files.

---
+++ JsonDataManager/Data/Wage.cs
@@ -0,0 +1,14 @@
+﻿namespace JsonDataManager.Data
+{
+    public class Wage
+    {
+        public double Gross { get; set; }
+        public double Net { get; set; }
+
+        public Wage(double gross, double net)
+        {
+            Gross = gross;
+            Net = net;
+        }
+    }
+}
---
+++ JsonDataManager/Data/WageService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json;
+using System.Xml.Linq;
+
+namespace JsonDataManager.Data
+{
+    public class WageService
+    {
+        public const double TAX_KEY = 0.665;
+        private const string FILE_PATH = @"wages.json";
+        public async Task GenerateWagesAsync(int count)
+        {
+            List<Wage> wages = new List<Wage>();
+            Random r = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                double wage = r.Next(200000, 5500000);
+                wages.Add(new Wage(wage, wage * TAX_KEY));
+            }
+            await using FileStream createStream = File.Create(FILE_PATH);
+            await JsonSerializer.SerializeAsync(createStream, wages);
+        }
+
+        public async Task<List<Wage>> GetWagesAsync()
+        {
+            await using FileStream openStream = File.OpenRead(FILE_PATH);
+            return await JsonSerializer.DeserializeAsync<List<Wage>>(openStream);
+        }
+    }
+}
